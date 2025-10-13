2f7147ad3ffddb1d30756cb843bc9b1548ff5e5d
Amr Foqha <amr.mofeed.foqha9@gmail.com>
2025-10-13 17:46:21 +0300

    login and Registration

---
+++ Python-Stack/django/django_fundamentals/Login_and_Registration/myapp/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+import re
+# Create your models here.
+
+class UserManager(models.Manager):
+    def basic_validator(self, postData,str):
+        errors={}
+        if str=='reg':
+            if(len(postData['first_name'])<2):
+                errors['first_name']="first name should be at least 2 characters"
+            if len(postData['last_name'])<2:
+                errors['last_name']="last name should be at least 2 characters"
+
+            if len(postData['email'])<=0:
+                errors['email']="Email should be filled"
+            elif not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', postData['email']):
+                errors['email1']="Email should be Valid"
+            elif if_email_not_unique(postData['email']):
+                errors['email2']="email already exists"
+            if len(postData['password'])<8 :
+                errors['password']="Password should be at least 8 characters"
+            if postData['password']!=postData['confirm_pw']  :
+                errors['confirm_password']="Passwords should be match"
+                
+        else:
+            if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', postData['email']):
+                errors['email']="Email should be Valid"
+            elif not search_user(postData['email']):    
+                errors['login']="Email provided not found"    
+            if len(postData['password'])<8 :
+                errors['password']="Password should be at least 8 characters"
+            if len(postData['password'])>=0 :
+                errors['password']="Please Enter Password"
+            
+            
+                
+        return errors
+                
+        
+
+
+
+class Users(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    email=models.CharField(max_length=50,unique=True)
+    password=models.CharField(max_length=100)
+    objects=UserManager()
+    
+    
+def create_user(first_name,last_name,email,password):
+    return Users.objects.create(first_name=first_name,last_name=last_name,email=email,password=password)
+ 
+def search_user(email):
+    return Users.objects.filter(email__exact=email)
+            
+def if_email_not_unique(email):
+    return Users.objects.filter(email__exact=email).exists()          
+
+def get_user(id):
+    return Users.objects.get(id=id)
---
+++ Python-Stack/django/django_fundamentals/Login_and_Registration/myapp/views.py
@@ -0,0 +1,73 @@
+import bcrypt
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib import messages
+
+# Create your views here.
+def root(request):
+                
+    if "user" not in request.session:
+        request.session['user_id']=None
+    if "logged_in" not in request.session:
+        request.session['logged_in']=False
+            
+    return render(request,'login.html')
+
+def check(request):
+    type=request.POST['hidden']
+    if type=="registration":
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        email=request.POST['email']
+        password=request.POST['password']
+        errors=Users.objects.basic_validator(request.POST,'reg')
+        if len(errors)>0:
+            for key,val in errors.items():
+                messages.error(request,val,'reg_{key}')
+            return redirect('/')  
+        else:  
+            hash1 = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+            
+            user=create_user(first_name,last_name,email,hash1)
+            request.session['user_id']=user.id
+            request.session['logged_in']=True
+            return redirect('/display_success')
+            
+    else:
+        errors=Users.objects.basic_validator(request.POST,'log')
+        if len(errors)>0:
+            for key,val in errors.items():
+                messages.error(request,val,'log_{key}')
+            return redirect('/')  
+        else:
+            email=request.POST['email']
+            password=request.POST['password']
+            user=search_user(email)
+            if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+                request.session['logged_in']=True
+                request.session['user_id']=user.id
+                return redirect('/display_success')
+            else:
+                messages.error(request,"Wrong password",'log_pass')
+                return redirect('/') 
+            
+def display_success(request):
+    if(request.session['logged_in']==True):
+       
+        context={
+            'user': get_user(request.session['user_id'])
+        }
+        return render(request,'success.html',context)
+    else:
+        return redirect('/')
+    
+def flush_session(request):
+    del request.session['logged_in']
+    del request.session['user_id']
+    return redirect('/')
+    
+
+                        
+            
+                
+      
