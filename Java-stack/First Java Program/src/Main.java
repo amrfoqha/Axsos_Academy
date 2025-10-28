0ea1c7fa648558acf94a71b5162c9c320262ad11
Amr Foqha <amr.mofeed.foqha9@gmail.com>
2025-10-28 14:41:14 +0200

    first java project

---
+++ Java-stack/First Java Program/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My Name is Amr Foqha");
+        System.out.println("iam 24 years old");
+        System.out.println("My hometown is Nablus, palestine");
+    }
+}
