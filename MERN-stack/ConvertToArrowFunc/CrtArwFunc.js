85f71cbd7a626db9577d7b698c14399f0e03e7f8
Amr Foqha <amr.mofeed.foqha@gmail.com>
2025-12-03 09:04:43 +0200

    ArrowFunc

---
+++ MERN-stack/ConvertToArrowFunc/CrtArwFunc.js
@@ -0,0 +1,49 @@
+// document.getElementById("button").onclick = function () {
+//   setBackgroundColorById("paragraph", "blue");
+// };
+// document.getElementById("alert").onclick = function () {
+//   alert(document.getElementById("popup-input").value);
+// };
+// document.getElementById("hover-this").onmouseover = function () {
+//   setBackgroundColorById("body", "red");
+// };
+// document.getElementById("hover-this").onmouseout = function () {
+//   setBackgroundColorById("body", "white");
+// };
+// function getValueFromId(id) {
+//   return document.getElementById(id).value;
+// }
+// function setBackgroundColorById(id, color) {
+//   document.getElementById(id).style = "background-color: " + color;
+// }
+// function mouseOverFunction(el) {
+//   el.style = "background-color: black";
+// }
+
+const buttonOnClick = () => {
+  var BackgroundColor = "blue";
+};
+
+const alertOnClick = () => {
+  return "Value";
+};
+
+const onMouseOver = () => {
+  var BackgroundColor = "red";
+};
+
+const onMouseOut = () => {  
+  var BackgroundColor = "white";
+};
+
+const getValueFromId = (id) => {
+  return document.getElementById(id).value;
+};
+
+const setBackgroundColorById = (id, color) => {
+  document.getElementById(id).style = "background-color: " + color;
+};
+
+const mouseOverFunction = (el) => {
+    el.style = "background-color: black";
+}
