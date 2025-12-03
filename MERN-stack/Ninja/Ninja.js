bcfd57fb85446f0359cca26cd634c30976ee60f2
Amr Foqha <amr.mofeed.foqha@gmail.com>
2025-12-03 19:06:02 +0200

    Create Ninja.js

---
+++ MERN-stack/Ninja/Ninja.js
@@ -0,0 +1,19 @@
+export class Ninja {
+  constructor(name, health) {
+    this.name = name;
+    this.health = health;
+    this.speed = 3;
+    this.strength = 3;
+  }
+  sayHello() {
+    console.log("The name of the ninja is: " + this.name);
+  }
+  showStats() {
+    console.log(
+      this.name + " " + this.strength + " " + this.speed + " " + this.health
+    );
+  }
+  drinkSake() {
+    this.health += 10;
+  }
+}
