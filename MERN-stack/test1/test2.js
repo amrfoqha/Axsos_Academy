02ee006c73e3bbeab5d06b038d3ba8553bc1df56
Amr Foqha <amr.mofeed.foqha@gmail.com>
2025-12-03 19:06:44 +0200

    Update test2.js

--- MERN-stack/test1/test2.js
+++ MERN-stack/test1/test2.js
@@ -1,1 +1,14 @@
-console.log("weqwewqee")
+const batata = (newstr) => {
+  var str = newstr.split("");
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+    [str[left], str[right]] = [str[right], str[left]];
+    left++;
+    right--;
+  }
+
+  console.log(str.join(""));
+};
+
+batata("ayham");
