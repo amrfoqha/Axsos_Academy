19c23f49dbd971cab3b4371076bd910291588ac3
Amr Foqha <amr.mofeed.foqha@gmail.com>
2025-12-06 19:59:03 +0200

    First JSX

---
+++ MERN-stack/First JSX/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h3>Things that i need to do.</h3>
+      <ul>
+        <li>learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run Marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
