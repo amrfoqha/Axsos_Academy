6887200db75b369fece158a9e592d517843ae238
Amr Foqha <amr.mofeed.foqha@gmail.com>
2025-12-09 19:40:37 +0200

    Update test2.js

--- MERN-stack/test1/test2.js
+++ MERN-stack/test1/test2.js
@@ -1,14 +1,28 @@
-const batata = (newstr) => {
-  var str = newstr.split("");
-  let left = 0;
-  let right = str.length - 1;
-  while (left < right) {
-    [str[left], str[right]] = [str[right], str[left]];
-    left++;
-    right--;
+// const batata = (newstr) => {
+//   var str = newstr.split("");
+//   let left = 0;
+//   let right = str.length - 1;
+//   while (left < right) {
+//     [str[left], str[right]] = [str[right], str[left]];
+//     left++;
+//     right--;
+//   }
+
+//   console.log(str.join(""));
+// };
+
+// batata("ayham");
+
+const palindrome = (str) => {
+  var arr = [];
+  for (let ch of str) {
+    arr.push(ch);
   }
-
-  console.log(str.join(""));
+  for (let index = 0; index < arr.length; index++) {
+    if (str[index] != arr.pop()) {
+      return false;
+    }
+  }
+  return true;
 };
-
-batata("ayham");
+console.log(palindrome("asasa"));
